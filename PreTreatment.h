2a44029988ecd5c52c17d0b8be09a973eb73f79b
AIT SEDDIK <yohann.ait-seddik@epita.fr>
2020-10-27 17:48:17 +0100

    FirstCommit

---
+++ PreTreatment.h
@@ -0,0 +1,7 @@
+#ifndef PRETRATMENT_H
+#define PRETRATMENT_H
+
+void AllTreatment(int a, int b, Uint8 Array[a][b], SDL_Surface* image);
+  
+#endif
+
---
+++ utility_SDL.h
@@ -0,0 +1,9 @@
+#ifndef UTILITY_SDL_H
+#define UTILITY_SDL_H
+
+Uint32 getpixel(SDL_Surface *surface, int x, int y);
+void putpixel(SDL_Surface *surface, int x, int y, Uint32 pixel);
+void display_Array(int h, int w, Uint8 binaryArray[h][w], SDL_Surface* image);
+void wait_for_keypressed();
+
+#endif
