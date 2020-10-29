03771385385bc1d7afbe7d2f425f8399caed9c6b
armand.mounsi <armand.mounsi@epita.fr>
2020-10-29 14:33:47 +0100

    Clean some useless stuff

--- PreTreatment.h
+++
@@ -1,7 +0,0 @@
-#ifndef PRETRATMENT_H
-#define PRETRATMENT_H
-
-void AllTreatment(int a, int b, Uint8 Array[a][b], SDL_Surface* image);
-  
-#endif
-
--- Segmentation.h
+++
@@ -1,6 +0,0 @@
-#ifndef SEGMENTATION_H
-#define SEGMENTATION_H
-
-void InitFile(int height,int width, Uint8 binaryArray[height][width],SDL_Surface * image_s);
-  
-#endif
