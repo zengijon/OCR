8276ee2ea4d09afd4f0cccb67a0e74bc6618f830
yohann.ait-seddik <yohann.ait-seddik@epita.fr>
2020-12-16 02:47:08 +0100

    Fix some interface problem, Add a Save folder for the OCR and a Binarization button

--- All/main.c
+++ All/main.c
@@ -39,13 +39,19 @@ GtkWidget *grid;
 GtkBuilder* builder;
 GtkWidget *image;
 GtkWidget *res;
+GError *error = NULL;
 SDL_Surface *surface;
 char *filename;
+
+void open_image(GtkWidget *image, char* filename)
+{
+	GdkPixbuf *pixbuf = gdk_pixbuf_new_from_file_at_scale(filename, 800, 650, FALSE,&error);
+	gtk_image_set_from_pixbuf(GTK_IMAGE(image), pixbuf);
+}
 
 void open_dialog(GtkWidget *window)
 {
 	GtkWidget *dialog;
-
 	dialog = gtk_file_chooser_dialog_new("Choose a file", GTK_WINDOW(window), GTK_FILE_CHOOSER_ACTION_OPEN, 
 		"_Open", GTK_RESPONSE_ACCEPT, "_Cancel", GTK_RESPONSE_CANCEL, NULL);
 	gtk_widget_show_all(dialog);
@@ -55,25 +61,40 @@ void open_dialog(GtkWidget *window)
 	{
 		GtkFileChooser *chooser = GTK_FILE_CHOOSER(dialog);
 		filename = gtk_file_chooser_get_filename(chooser);
-		gtk_image_set_from_file(GTK_IMAGE(image), filename);
-		gtk_image_set_from_file(GTK_IMAGE(res), filename);
+		open_image(image, filename);
+		open_image(res, filename);
+		surface = load_image(filename);
 	}
     gtk_widget_destroy(dialog);
 }
 
+void binarize_image()
+{
+    int h = surface->h;
+    int w = surface->w;
+    Uint8 binaryArray[h][w];
+    AllTreatment(h, w, binaryArray, surface, "b");
+    display_Array(h, w, binaryArray, surface);
+    IMG_SavePNG(surface, "Save/res.png");
+    open_image(res, "Save/res.png");
+}
+
 void init_window()
 {
-	GtkWidget *button;
+	GtkWidget *Open_file;
+	GtkWidget *Binarize;
 
 	window = GTK_WIDGET(gtk_builder_get_object(builder, "Main_window"));
-	
 	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-	button = GTK_WIDGET(gtk_builder_get_object(builder, "Open"));
-	g_signal_connect(GTK_BUTTON(button), "activate", G_CALLBACK(open_dialog), NULL);
+	Open_file = GTK_WIDGET(gtk_builder_get_object(builder, "Open"));
+	g_signal_connect(GTK_BUTTON(Open_file), "activate", G_CALLBACK(open_dialog), NULL);
 
 	image = GTK_WIDGET(gtk_builder_get_object(builder, "image"));
 	res = GTK_WIDGET(gtk_builder_get_object(builder, "res"));
+
+	Binarize = GTK_WIDGET(gtk_builder_get_object(builder, "Binarize"));
+	g_signal_connect(GTK_BUTTON(Binarize), "activate", G_CALLBACK(binarize_image), NULL);
 }
 
 void freeS()
@@ -97,11 +118,6 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 	init_window();
-
-
-
-
-
 
 	gtk_widget_show_all(window);
 	gtk_main();
