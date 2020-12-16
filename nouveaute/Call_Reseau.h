ecb4b4eaa9d386026540326b8fe59d1f79c20cb7
Clousty8 <armand.mounsi@gmail.com>
2020-12-16 19:43:05 +0100

    nouveaute added again

---
+++ nouveaute/Call_Reseau.h
@@ -0,0 +1,6 @@
+#ifndef CALL_RESEAU_H
+#define CALL_RESEAU_H
+
+void handle_network(int h,int w, Uint8 binaryArray[h][w]);
+  
+#endif
---
+++ nouveaute/Reseau.h
@@ -0,0 +1,6 @@
+#ifndef RESEAU_H
+#define RESEAU_H
+
+char Reseau(float image[28][28],char c);
+
+#endif
